26647684904f1f8782d3c020ac5ed94489805864
sh526033@dal.ca <shubhamjethva92@gmail.com>
2024-11-08 02:08:51 -0400

    run

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/shubham-910/shubham-910.github.io'
-})
+  base: '/shubham-910.github.io/' // Add leading and trailing slashes
+});
