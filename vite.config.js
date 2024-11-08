541d689518c8bd66c200c0fd4e7d107f1d9f97b8
sh526033@dal.ca <shubhamjethva92@gmail.com>
2024-11-08 02:11:36 -0400

    run

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/shubham-910.github.io/' // Add leading and trailing slashes
+  plugins: [react()]
 });
